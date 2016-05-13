262670d780523ebf50fd2b55fc65525d81bfbb2c
owlman <jie.owl2008@gmail.com>
2016-05-14 04:23:33 +0800

    some changes...

--- Git/git_pull_remote.py
+++ Git/git_pull_remote.py
@@ -10,14 +10,14 @@ import os
 import sys
 
 if not len(sys.argv) in range(1, 4):
-    print "Usage: git_pull_remote.py <git_dir> [branch]" 
+    print("Usage: git_pull_remote.py <git_dir> [branch]") 
     exit()
 
 title = "= Starting " + sys.argv[0] + "... ="
 n = len(title)
-print n*'='
-print title
-print n*'='
+print(n*'=')
+print(title)
+print(n*'=')
 
 if len(sys.argv) == 3 and sys.argv[2] != "":
 	branch = sys.argv[2]
@@ -25,14 +25,14 @@ else :
 	branch = "master"
 
 os.chdir(sys.argv[1])
-print "PWD: "+ os.popen("pwd").readline()
+print("PWD: "+ os.popen("pwd").readline())
 
 for remote in os.popen("git remote show").readlines():
-    print ""
-    print "Pulling from " + remote[0:-1] + "..."
+    print("")
+    print("Pulling from " + remote[0:-1] + "...")
     os.system("git pull " + remote[0:-1] + " " + branch)
-    print "Pull is complete!"
+    print("Pull is complete!")
 
-print n*'='    
-print "= Done!" + (n-len("= Done!")-1)*' ' + "="
-print n*'='
+print(n*'=')    
+print("= Done!" + (n-len("= Done!")-1)*' ' + "=")
+print(n*'=')
