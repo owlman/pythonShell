5d477fdab9c28a62b5e9449682d0549281bf04a3
owlman <jie.owl2008@gmail.com>
2016-06-12 20:53:52 +0800

    added hello_python.py...

---
+++ helloworld/hello_python.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+'''
+    Created on 2016-6-12
+    
+    @author: lingjie
+    @name:   hello_python 
+'''
+def main():
+	print("hello world!")
+
+if __name__ == '__main__':
+	main()
