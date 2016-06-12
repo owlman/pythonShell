e10284c15835add07410c185aa8f3fb94c53846b
owlman <jie.owl2008@gmail.com>
2016-06-12 20:40:32 +0800

    added hello_cpp.py...

--- helloworld/hello_cpp.py
+++ helloworld/hello_cpp.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 '''
-    Created on 2016-6-5
+    Created on 2016-6-12
     
     @author: lingjie
-    @name:   hello_c
+    @name:   hello_cpp
 '''
 import os
 import sys
@@ -11,21 +11,21 @@ import shutil
 import platform
 
 if not len(sys.argv) in range(2, 3):
-    print("Usage: hello_c.py <compiler>") 
+    print("Usage: hello_cpp.py <compiler>") 
     exit(1)
 
 if(not os.path.exists("example")):
 	os.mkdir("example")
-	codepath = sys.path[0] + "/src/test_c.c"
+	codepath = sys.path[0] + "/src/test_cpp.cpp"
 	if(os.path.exists(codepath)):
-		shutil.copy(codepath,"example/hello.c")
+		shutil.copy(codepath,"example/hello.cpp")
 	else:
-		code = "#include <stdio.h>\nint main(){printf(\"hello world!\\n\");return 0;} "
-		file = open(r"example/hello.c",'w')
+		code = "#include <iostream>\nusing namespace std;int main(void){cout<<\"hello world!\\n\"; return 0;}"
+		file = open(r"example/hello.cpp",'w')
 		file.writelines(code)
 		file.close()
 
-cmd = sys.argv[1] + r" example/hello.c -o example/test.exe"
+cmd = sys.argv[1] + r" example/hello.cpp -o example/test.exe"
 os.system(cmd)
 
 sysstr = platform.system()
