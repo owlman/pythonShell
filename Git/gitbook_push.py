935629c10669f3cad1200cf88c8d2d6afe68235e
owlman <jie.owl2008@gmail.com>
2019-06-19 15:17:30 +0800

    some change

--- Git/gitbook_push.py
+++ Git/gitbook_push.py
@@ -15,18 +15,18 @@ if not len(sys.argv) in range(2, 4):
 
 title = "=    Starting " + sys.argv[0] + "......    ="
 n = len(title)
-print(n*'=')
+print(n * '=')
 print(title)
-print(n*'=')
+print(n * '=')
 
 os.chdir(sys.argv[1])
-print("work_dir: " + sys.argv[1])
+print("wor`k_dir: " + sys.argv[1])
 
 os.system("gitbook build")
 
 if len(sys.argv) == 3 and sys.argv[2] != "":
     os.system("git add .")
-    os.system("git commit -m '"+sys.argv[2]+"'")
+    os.system("git commit -m '" + sys.argv[2] + "'")
 
 if os.path.exists("../_book"):
     os.system("rm -rf ../_book")
@@ -38,10 +38,9 @@ os.system("cp -r ../_book/* ./")
 
 if len(sys.argv) == 3 and sys.argv[2] != "":
     os.system("git add .")
-    os.system("git commit -m '"+sys.argv[2]+"'")
+    os.system("git commit -m '" + sys.argv[2] + "'")
 
-
- for remote in os.popen("git remote show").readlines():
+for remote in os.popen("git remote show").readlines():
     print("")
     print("Pushing to " + remote[0:-1] + "...")
     os.system("git push -u " + remote[0:-1])
@@ -49,6 +48,6 @@ if len(sys.argv) == 3 and sys.argv[2] != "":
 
 os.system("git checkout master")
 
-print(n*'=')    
+print(n * '=')
 print("=     Done!" + (n-len("=     Done!")-1)*' ' + "=")
-print(n*'=')
+print(n * '=')
